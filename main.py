6ea9bb12c4c2f96192a05cb3c5ce8855b73945c6
Mohammad-Hossein Kiahosseini <kiahosseini.smh@gmail.com>
2025-01-21 16:54:28 +0330

    add some extra info

--- main.py
+++ main.py
@@ -1,48 +1,95 @@
 import os
+import re
 import pandas as pd
-import numpy as np
-import json
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier, export_text, plot_tree
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier
-from xgboost import XGBClassifier
-import xgboost as xgb
-# from lightgbm
+import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report, recall_score, precision_score, f1_score
-from imblearn.over_sampling import SMOTE
-from openpyxl import Workbook
+
+
+def split_camel_case(text):
+    return re.sub(r'(?<!^)(?=[A-Z])', ' ', text)
+
 
 def main():
-  # Load data
-  data = pd.read_csv(os.path.join(os.path.dirname(__file__), 'alzheimers_disease_data.csv'))
-  
-  data = data.drop(columns=['DoctorInCharge'])
-  # Replace Ethnicity data with meaningful data
-  
-  # Translating from numbers to words
-  # 0 => Caucasian
-  # 1 => African American
-  # 2 => Asian
-  # 3 => Other
-  
-  data['Ethnicity'] = data['Ethnicity'].replace([0,1,2,3], ['Caucasian', 'African_American', 'Asian', 'Other'])
+    # Load data
+    data = pd.read_csv(os.path.join(os.path.dirname(
+        __file__), 'alzheimers_disease_data.csv'))
 
-  # Normalize features
-  scaler = MinMaxScaler()
-  data_scaled = scaler.fit_transform(data.drop(columns=['Diagnosis']))
+    data.head().T
 
-  # Separate features and targets
-  X = data.drop(columns=['Diagnosis'])
-  y = data['Diagnosis']
+    data.info()
 
-  label_encoder = LabelEncoder()
-  y_encoded = label_encoder.fit_transform(y)
-  
-  # Stratified train-test split
-  X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, stratify=y_encoded, random_state=42)
-  
-  
+    data.describe().T
+
+    sum(data.duplicated())
+
+    data = data.drop(columns=['PatientID', 'DoctorInCharge'])
+
+    # Identify numerical columns: columns with more than 10 unique values are considered numerical
+    numerical_columns = [
+        col for col in data.columns if data[col].nunique() > 10]
+
+    # Identify categorical columns: columns that are not numerical and not 'Diagnosis'
+    categorical_columns = data.columns.difference(
+        numerical_columns).difference(['Diagnosis']).to_list()
+
+    custom_labels = {
+        'Gender': ['Male', 'Female'],
+        'Ethnicity': ['Caucasian', 'African American', 'Asian', 'Other'],
+        'EducationLevel': ['None', 'High School', 'Bachelor\'s', 'Higher'],
+        'Smoking': ['No', 'Yes'],
+        'FamilyHistoryAlzheimers': ['No', 'Yes'],
+        'CardiovascularDisease': ['No', 'Yes'],
+        'Diabetes': ['No', 'Yes'],
+        'Depression': ['No', 'Yes'],
+        'HeadInjury': ['No', 'Yes'],
+        'Hypertension': ['No', 'Yes'],
+        'MemoryComplaints': ['No', 'Yes'],
+        'BehavioralProblems': ['No', 'Yes'],
+        'Confusion': ['No', 'Yes'],
+        'Disorientation': ['No', 'Yes'],
+        'PersonalityChanges': ['No', 'Yes'],
+        'DifficultyCompletingTasks': ['No', 'Yes'],
+        'Forgetfulness': ['No', 'Yes']
+    }
+
+    for column in categorical_columns:
+        plt.figure(figsize=(8, 5))
+        sns.countplot(data=data, x=column)
+        plt.title(f'Countplot of {column}')
+
+        manager = plt.get_current_fig_manager()
+        title = split_camel_case(column)
+        manager.set_window_title(title)
+
+        # Directly set custom labels
+        labels = custom_labels[column]
+        ticks = range(len(labels))
+        plt.xticks(ticks=ticks, labels=labels)
+
+        plt.show()
+
+    # # Normalize features
+    # scaler = MinMaxScaler()
+    # data_scaled = scaler.fit_transform(data.drop(columns=['Diagnosis']))
+
+    # # Separate features and targets
+    # X = data.drop(columns=['Diagnosis'])
+    # y = data['Diagnosis']
+
+    # label_encoder = LabelEncoder()
+    # y_encoded = label_encoder.fit_transform(y)
+
+    # # Stratified train-test split
+    # X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, stratify=y_encoded, random_state=42)
+
+    # plt.figure(figsize=(8,6))
+    # sns.heatmap(data, annot=True, cmap="YlGnBu", linewidths=0.5)
+
+    # plt.title("Heatmap Chart")
+
+    # plt.show()
+
 
 main()
